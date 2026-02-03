cdb5e904829b53ab03388704b44be54698dfbd29
MEGAB0G <megabog@users.noreply.github.com>
2026-02-03 11:47:14 +0500

    update sftp setup

---
+++ topm/topm.js
@@ -0,0 +1,8 @@
+const navToggle = document.querySelector('[data-topm-toggle]');
+const nav = document.querySelector('.topm-nav');
+
+if (navToggle && nav) {
+    navToggle.addEventListener('click', () => {
+        nav.classList.toggle('is-open');
+    });
+}
